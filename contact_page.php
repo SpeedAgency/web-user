38dfbdfdc56113bf8e05eed00d21e5225477cd10
SpeedAgency <steve@speed.agency>
2015-05-05 15:46:21 +0100

    LatLng
    Added define’s and comments.

--- contact_page.php
+++ contact_page.php
@@ -26,6 +26,8 @@
     <div id="map-canvas"></div>
     <div id="panel">
       <input id="address" type="text" name="location[address]" value="<?php echo esc_attr($location_opt['address']); ?>" class="form-control">
+        
+      <?php // Add Latitude and Longitide Form Fields. These will be saved to the $location array. ?>
       <input id="address_lat" type="hidden" name="location[lat]" value="<?php echo esc_attr($location_opt['lat']); ?>">
       <input id="address_lng" type="hidden" name="location[lng]" value="<?php echo esc_attr($location_opt['lng']); ?>">
       <input type="button" value="Geocode" onclick="codeAddress()" class="button-primary">
@@ -56,22 +58,29 @@ register_setting( 'my-plugin-settings-group', 'location');
 	    define('user_email', 'email here');
    }
    if( get_option('contact_deets_phone')){
-    $user_phone = get_option('contact_deets_phone');
-    define('user_phone', $user_phone);
+        $user_phone = get_option('contact_deets_phone');
+        define('user_phone', $user_phone);
     } else {
 	    define('user_phone', 'Phone number here');
    }
     if( get_option('contact_deets_address')){
-    $user_address = get_option('contact_deets_address');
-    define('user_address', $user_address);
+        $user_address = get_option('contact_deets_address');
+        define('user_address', $user_address);
     } else {
 	    define('user_address', 'address here');
    }
+
+
+    // Setup the defines for the $location array. Remember to add default fallbacks. 
     if( get_option('location')){
-    $location = get_option('location');
-    define('location_address', $location['address']);
+        $location = get_option('location');
+        define('location_address', $location['address']);
+        define('location_lat', $location['lat']);
+        define('location_lng', $location['lng']);
     } else {
 	    define('location_address', 'location here');
+        define('location_lat', 0);
+        defind('location_lng', 0);
    }
    
    /** DEFINE ALL DATA END **/
@@ -97,8 +106,12 @@ function codeAddress() {
   geocoder.geocode( { 'address': address}, function(results, status) {
     if (status == google.maps.GeocoderStatus.OK) {
       map.setCenter(results[0].geometry.location);
+        
+      <?php // Get the value of Lat and Lng for the form fields. ?>    
       document.getElementById("address_lat").value = results[0].geometry.location['A'];
       document.getElementById("address_lng").value = results[0].geometry.location['F'];
+        
+        
       var marker = new google.maps.Marker({
           map: map,
           position: results[0].geometry.location
